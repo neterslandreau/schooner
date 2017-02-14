6922f972878c30b2fc7781db7c81499dcbb397af
Niles Rowland <neterslandreau@gmail.com>
2017-02-14 13:41:44 -0500

    get rid of so many sidebar choices for demo.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,11 +2,4 @@
     <a href="#" class="list-group-item active">Link</a>
     <a href="#" class="list-group-item">Link</a>
     <a href="#" class="list-group-item">Link</a>
-    <a href="#" class="list-group-item">Link</a>
-    <a href="#" class="list-group-item">Link</a>
-    <a href="#" class="list-group-item">Link</a>
-    <a href="#" class="list-group-item">Link</a>
-    <a href="#" class="list-group-item">Link</a>
-    <a href="#" class="list-group-item">Link</a>
-    <a href="#" class="list-group-item">Link</a>
   </div>
