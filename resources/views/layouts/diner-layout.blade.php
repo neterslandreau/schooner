67836f160425d055b91d4b3d367368b7bba4f74f
Niles Rowland <neterslandreau@gmail.com>
2017-02-23 07:42:21 -0500

    add dismiss buttons to the flash messages.

--- resources/views/layouts/diner-layout.blade.php
+++ resources/views/layouts/diner-layout.blade.php
@@ -30,16 +30,18 @@
     <div class="container">
     @if ($flash = session('message'))
 
-    <div class="alert alert-success" role="alert">
-        {{ $flash }}
+    <div class="alert alert-success alert-dismissable" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            {{ $flash }}
     </div>
 
     @endif
 
     @if ($flash = session('error'))
 
-    <div class="alert alert-danger" role="alert">
-        {{ $flash }}
+    <div class="alert alert-danger alert-dismissable" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            {{ $flash }}
     </div>
 
     @endif
