58424f7f34218e9b09d23ef1fc1db2532cb1aab2
Niles Rowland <neterslandreau@gmail.com>
2017-02-28 12:44:39 -0500

    fix broken image links.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,17 +2,25 @@
   <div class="list-group">
     @foreach ($breakfastItems as $item)
 
+        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+
+
     @if (\File::exists(public_path().'/storage/items/'.$item->slug.'.png'))
 
         <?php $item->image = '/storage/items/'.$item->slug.'.png'; ?>
 
+        <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
+
+    @else
+
+    {{ $item->name }} {{ $item->price }}
+
     @endif
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
-            <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
-        </a>
 
     @endforeach
+
+        </a>
 
   </div>
 
@@ -20,50 +28,72 @@
   <div class="list-group">
     @foreach ($lunchItems as $item)
 
+        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+
+
     @if (\File::exists(public_path().'/storage/items/'.$item->slug.'.png'))
 
         <?php $item->image = '/storage/items/'.$item->slug.'.png'; ?>
 
+        <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
+
+    @else
+
+    {{ $item->name }} {{ $item->price }}
+
     @endif
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
-            <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
-        </a>
 
     @endforeach
+
+        </a>
   </div>
 
   <h2>Smoothies</h2>
   <div class="list-group">
     @foreach ($smoothies as $item)
+        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+
+
     @if (\File::exists(public_path().'/storage/items/'.$item->slug.'.png'))
 
         <?php $item->image = '/storage/items/'.$item->slug.'.png'; ?>
 
+        <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
+
+    @else
+
+    {{ $item->name }} {{ $item->price }}
+
     @endif
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
-            <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
-        </a>
 
-        @include('partials.modal')
     @endforeach
+
+        </a>
   </div>
 
   <h2>Beverages</h2>
   <div class="list-group">
     @foreach ($drinks as $item)
+        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+
+
     @if (\File::exists(public_path().'/storage/items/'.$item->slug.'.png'))
 
         <?php $item->image = '/storage/items/'.$item->slug.'.png'; ?>
 
+        <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
+
+    @else
+
+    {{ $item->name }} {{ $item->price }}
+
     @endif
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
-            <img height="25" src="{{ $item->image }}"> {{ $item->name }} ${{ $item->price }}
-        </a>
 
-        @include('partials.modal')
     @endforeach
+
+        </a>
   </div>
 
