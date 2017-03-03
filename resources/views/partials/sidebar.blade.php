a4ae9294cb245566834028887bd81d7b8529615b
Niles Rowland <neterslandreau@gmail.com>
2017-03-03 12:18:52 -0500

    alter view on sidebar.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,82 +1,89 @@
-  <h2>Breakfasts</h2>
+<h2>Breakfasts</h2>
+<div class="list-group">
+@foreach ($breakfastItems as $item)
 
-  <div class="list-group">
-    @foreach ($breakfastItems as $item)
+    <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+        @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
 
-            @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
+        <div class="col-md-12">
+            <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" width="175">
+        </div>
 
-                <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" height="25">
+        @endif
 
-            @endif
+        {{ $item->name }} {{ $item->price }}
 
-            {{ $item->name }} {{ $item->price }}
+    </a>
 
-        </a>
+@endforeach
+</div>
 
-    @endforeach
-  </div>
+<h2>Lunches</h2>
+<div class="list-group">
+@foreach ($lunchItems as $item)
 
-  <h2>Lunches</h2>
-  <div class="list-group">
-    @foreach ($lunchItems as $item)
+    <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+        @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
 
-            @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
+        <div class="col-md-12">
+            <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" width="175">
+        </div>
 
-                <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" height="25">
+        @endif
 
-            @endif
+        {{ $item->name }} {{ $item->price }}
 
-            {{ $item->name }} {{ $item->price }}
+    </a>
 
-        </a>
+@endforeach
 
-    @endforeach
+</div>
 
-  </div>
+<h2>Smoothies</h2>
+<div class="list-group">
 
-  <h2>Smoothies</h2>
-  <div class="list-group">
+@foreach ($smoothies as $item)
 
-    @foreach ($smoothies as $item)
+    <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+        @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
 
-            @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
+        <div class="col-md-12">
+            <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" width="175">
+        </div>
 
-                <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" height="25">
+        @endif
 
-            @endif
+        {{ $item->name }} {{ $item->price }}
 
-            {{ $item->name }} {{ $item->price }}
+    </a>
 
-        </a>
-        
-    @endforeach
+@endforeach
 
-  </div>
+</div>
 
-  <h2>Beverages</h2>
-  <div class="list-group">
+<h2>Beverages</h2>
+<div class="list-group">
 
-    @foreach ($drinks as $item)
+@foreach ($drinks as $item)
 
-        <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
+    <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
 
-            @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
+        @if (\Storage::disk('s3')->exists('items/' . $item->slug . '.png'))
 
-                <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" height="25">
+        <div class="col-md-12">
+            <img src="//diner.think-knot.com.s3.amazonaws.com/items/{{ $item->slug }}.png" width="175">
+        </div>
 
-            @endif
+        @endif
 
-           {{ $item->name }} {{ $item->price }}
+       {{ $item->name }} {{ $item->price }}
 
-        </a>
+    </a>
 
-    @endforeach
+@endforeach
 
-  </div>
+</div>
 
