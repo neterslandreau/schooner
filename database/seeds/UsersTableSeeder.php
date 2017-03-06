8ead070adbfd9f32a9ea2a99a239349573ee7de4
Niles Rowland <neterslandreau@gmail.com>
2017-03-06 10:36:10 -0500

    switch to tabs.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,23 +5,23 @@ use Webpatser\Uuid\Uuid;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('users') ->insert([
-        	'id' => Uuid::generate()->string,
-        	'first_name' => 'Admin',
-        	'last_name' => 'Istrator',
-        	'username' => 'administrator',
-        	'email' => 'admin@think-knot.com',
-        	'role' => 'admin',
-        	'password' => bcrypt('secret'),
-        	'created_at' => date('Y-m-d H:i:s'),
-        	'updated_at' => date('Y-m-d H:i:s'),
-        ]);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('users') ->insert([
+			'id' => Uuid::generate()->string,
+			'first_name' => 'Admin',
+			'last_name' => 'Istrator',
+			'username' => 'administrator',
+			'email' => 'admin@think-knot.com',
+			'role' => 'admin',
+			'password' => bcrypt('secret'),
+			'created_at' => date('Y-m-d H:i:s'),
+			'updated_at' => date('Y-m-d H:i:s'),
+		]);
+	}
 }
