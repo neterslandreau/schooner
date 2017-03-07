fbde4cf61dd6bf39aefdb74fa59ffcf8afe296f9
Niles Rowland <neterslandreau@gmail.com>
2017-03-07 08:14:02 -0500

    switch to tabs.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,37 +4,37 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        view()->composer('home', function($view) {
-            $items = \App\Item::get();
-            $types = \App\Type::get();
-            $view->with(compact('items', 'types'));
-        });
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		view()->composer('home', function($view) {
+			$items = \App\Item::get();
+			$types = \App\Type::get();
+			$view->with(compact('items', 'types'));
+		});
 
-        view()->composer('diner', function($view) {
-            $featuredItems = \App\Item::featuredItems();
-            $breakfastItems = \App\Item::getItems('breakfast');
-            $lunchItems = \App\Item::getItems('lunch');
-            $smoothies = \App\Item::getItems('smoothies');
-            $drinks = \App\Item::getItems('drinks');
-            $view->with(compact('featuredItems', 'smoothies', 'lunchItems', 'breakfastItems', 'drinks'));
-        });
+		view()->composer('diner', function($view) {
+			$featuredItems = \App\Item::featuredItems();
+			$breakfastItems = \App\Item::getItems('breakfast');
+			$lunchItems = \App\Item::getItems('lunch');
+			$smoothies = \App\Item::getItems('smoothies');
+			$drinks = \App\Item::getItems('drinks');
+			$view->with(compact('featuredItems', 'smoothies', 'lunchItems', 'breakfastItems', 'drinks'));
+		});
 
-    }
+	}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 }
