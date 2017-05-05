b940cfb28a835b25d979af9383f861d5e2f51eb4
Niles Rowland <neterslandreau@gmail.com>
2017-05-05 09:25:59 -0400

    add safety check to delete type.

--- resources/views/partials/admin-types.blade.php
+++ resources/views/partials/admin-types.blade.php
@@ -1,10 +1,13 @@
+<?php
+	$requiredTypes = ['breakfast', 'lunch', 'drinks', 'smoothies'];
+?>
 <table class="table table-striped table-bordered table-condensed">
 	<thead>
 		<tr>
 			<td>Name</td>
 			<td>Description</td>
 			<td>Edit</td>
-			<!-- <td>Delete</td> -->
+			<td>Delete</td>
 		</tr>
 	</thead>
 	<tbody>
@@ -15,7 +18,11 @@
 			<td>{{ $type->name }}</td>
 			<td>{{ $type->description }}</td>
             <td><a class="glyphicon glyphicon-pencil nav-link" href="/types/{{ $type->slug }}/edit" role="button"><span></span></a></td>
-            <!-- <td><a class="glyphicon glyphicon-trash nav-link" href="/types/{{ $type->slug }}/delete" role="button"><span></span></a></td> -->
+            @if (in_array($type->slug, $requiredTypes))
+	            <td><a class="glyphicon glyphicon-trash nav-link" href="/types/{{ $type->slug }}/delete" role="button" onClick="return confirm('Are you absolutely sure? The site will break if this is deleted!')"><span></span></a></td>
+	         @else
+	            <td><a class="glyphicon glyphicon-trash nav-link" href="/types/{{ $type->slug }}/delete" role="button" onClick="return confirm('Are you absolutely sure?')"><span></span></a></td>
+            @endif
 		</tr>
 
 	@endforeach
