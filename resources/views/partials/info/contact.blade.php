892e5bb4ada76e9e5a1329b45ceb0fbc84f7e3eb
Niles Rowland <neterslandreau@gmail.com>
2017-05-05 10:07:49 -0400

    remove second phone number.

--- resources/views/partials/info/contact.blade.php
+++ resources/views/partials/info/contact.blade.php
@@ -1,4 +1,4 @@
 <div class="text-primary"><b>{{ config('app.name') }}</b></div>
 <div class="text-muted">{{ config('app.address') }}</div>
 <div class="text-danger">Hours of operation: {{ config('app.hours') }}</div>
-<div class="text-success"><b><a href="tel:+19548226840">954-822-6840</a> / <a href="tel:+19545495254">954-549-5264</a></b></div>
+<div class="text-success"><b><a href="tel:+19548226840">954-822-6840</a></b></div>
