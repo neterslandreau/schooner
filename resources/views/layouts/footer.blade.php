8a48f4b5534a0651730b8ce073f007d1bb2da122
Niles Rowland <neterslandreau@gmail.com>
2017-05-27 12:22:03 -0400

    refactor marketing.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,4 @@
 <footer>
-	<p class="centered">&copy; <?php echo date('Y'); ?> Cafe Solo, LLC.</p>
-	<p class="centered">Hosting provided by <a href="http://think-knot.com" target="_blank">The Neters Group</a></p>
+	<p class="centered">&copy; <?php echo date('Y'); ?> Cafe Solo, LLC. Hosting provided by <a href="http://think-knot.com" target="_blank">The Neters Group</a></p>
 </footer>
 
