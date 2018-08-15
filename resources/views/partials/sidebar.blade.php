9c2271926bb42131308e75d798010af4606dafa3
Niles Rowland <neterslandreau@gmail.com>
2018-08-15 11:00:55 -0400

    add required model partial.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -17,6 +17,8 @@
 
     </a>
 
+    @include('partials.modal')
+
 @endforeach
 </div>
 
@@ -37,6 +39,8 @@
         {{ $item->name }} {{ $item->price }}
 
     </a>
+
+    @include('partials.modal')
 
 @endforeach
 
