12edff8cd32e18992044285d08e3a00d5e4fbfc7
Niles Rowland <neterslandreau@gmail.com>
2018-08-21 15:50:49 -0400

    add storing invoices.

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\URL;
 use App\Mail\Order;
 use App\Invoice;
 use \Stripe\Stripe;
-// use Cartalyst\Stripe\Stripe;
 use net\authorize\api\contract\v1 as AnetAPI;
 use net\authorize\api\controller as AnetController;
 
@@ -32,7 +31,7 @@ class InvoicesController extends Controller
 	 */
 	public function create()
 	{
-		//
+		dd('here');
 	}
 
 	/**
@@ -106,16 +105,28 @@ class InvoicesController extends Controller
 		    'receipt_email' => request('stripeEmail'),
 		    'source' => $token,
 		]);
+
+		$email = request('stripeEmail');
+		$name = request('name');
+		$phone = request('phone');
+		$chargeType = 'card';
+
+
+		$invoice = Invoice::create([
+			'phone' => $phone,
+			'name' => $name,
+			'email' => $email,
+			'charge_type' => $chargeType,
+			'total' => \Cart::total(),
+
+		]);
+
 		$order = new Order(request('phone'), request('name'));
 
 		Mail::to(env('DINER_EMAIL'))
 			->bcc('diner@think-knot.com')
 			->send($order);
-		// dd($order);
-		// \SMS::send('my message', null, function($sms) {
-		// 	$sms->attachImage('/dev/null');
-		// 	$sms->to('+19545886692', 'verizonwireless');
-		// });
+
 		\Cart::clean();
 		session()->flash('message', 'Your order has been placed. Thank you, '.request('name').'!');
 		return redirect()->to('/');
