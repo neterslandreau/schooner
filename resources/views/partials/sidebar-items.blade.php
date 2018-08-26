8eea64935feb16e44a953830da5b63817531e414
Niles Rowland <neterslandreau@gmail.com>
2018-08-26 13:03:38 -0400

    refactor sidebar menu.

--- resources/views/partials/sidebar-items.blade.php
+++ resources/views/partials/sidebar-items.blade.php
@@ -2,19 +2,7 @@
 
 	@foreach ($menuItem as $mi => $item)
 
-    <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">
-
-        <div class="col-xs-10 col-md-12">
-
-        @if (Storage::disk('local')->exists('public/items/' . $item->slug . '.png'))
-
-
-        @endif
-
-        </div>
-        {{ $item->name }} <span class="pull-right">${{ $item->price }}</span>
-
-    </a>
+    <a class="list-group-item" data-toggle="modal" data-target="#{{ $item->slug }}">{{ $item->name }} <span class="pull-right">${{ $item->price }}</span></a>
 
     @include('partials.modal')
 
