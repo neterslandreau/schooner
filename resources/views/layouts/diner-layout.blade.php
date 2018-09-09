d915d895c72f4f08d19014fd13b939dcce94e8ed
Niles Rowland <neterslandreau@gmail.com>
2018-09-09 12:42:46 -0400

    add google maps api async.

--- resources/views/layouts/diner-layout.blade.php
+++ resources/views/layouts/diner-layout.blade.php
@@ -52,7 +52,7 @@
 
     </div>
 
-    <script src='//maps.google.com/maps/api/js?key=AIzaSyA4ElrMPUX1d9KEt-fIEwsDICCdNitjw3o'></script>
+    <script async defer src='//maps.google.com/maps/api/js?key=AIzaSyA4ElrMPUX1d9KEt-fIEwsDICCdNitjw3o'></script>
     <script src="{{ asset('js/app.js') }}"></script>
   </body>
 </html>
