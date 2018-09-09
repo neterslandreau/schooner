05f5d177ba84bb4c2acc4f77230eed8ba4caf56b
Niles Rowland <neterslandreau@gmail.com>
2018-09-09 13:33:04 -0400

    cahnge footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer>
-	<p class="text-center">&copy; <?php echo date('Y'); ?> Hosting provided by <a href="http://think-knot.com" target="_blank">The Neters Group</a></p>
+	<p class="text-center">&copy; <?php echo date('Y'); ?> Menu powered by <a href="http://think-knot.com" target="_blank">The Neters Group</a></p>
 </footer>
 
