aff6ca336fd04115bad43081ef7cc253c37241d3
Niles Rowland <neterslandreau@gmail.com>
2018-09-10 11:20:49 -0400

    start calling the app eMenu.

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -13,7 +13,7 @@
 
 			<div class="modal-body">
 
-				<p class="text-center">&copy; <?php echo date('Y'); ?> Menu powered by <a href="http://think-knot.com" target="_blank">The Neters Group</a></p>
+				<p class="text-center">&copy; <?php echo date('Y'); ?> eMenu powered by <a href="http://think-knot.com" target="_blank">The Neters Group</a></p>
 
 			</div>
 
