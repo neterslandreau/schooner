433b267ee90b3ce2be8eee19b3fcbe668a679a58
Niles Rowland <neterslandreau@gmail.com>
2018-09-22 12:38:02 -0400

    reformat admin page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 <div class="container-fluid">
-    <button type="button" class="btn btn-primary" id="toggle-panels">Toggle</button>
-    <div class="row">
+    <div class="col-xs-12 col-md-12">
+        <button type="button" class="btn btn-primary" id="toggle-panels">Menu Items</button>
+        <button type="button" class="btn btn-primary" id="toggle-panels">Menu Types</button>
+        <button type="button" class="btn btn-primary" id="toggle-panels">Site Configuration</button>
+    </div>
 
         <div id="site-configuration" style="display:none;">
             <div class="col-xs-12 col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Site Configuration</div>
+                    <div class="panel-heading">Menu Types</div>
                     <div class="panel-body" style="overflow: scroll;">
-                            @include('partials.admin-types')
-                            @include('partials.admin-config')
+                        @include('partials.admin-types')
                     </div>
                 </div>
             </div>
@@ -28,6 +30,17 @@
                 </div>
             </div>
         </div>
-   </div>                 
+
+        <div id="site-projects" style="display:block;">
+            <div class="col-xs-12 col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Menu Items</div>
+                    <div class="panel-body" style="overflow: scroll;">
+                        @include('partials.admin-config')
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </div>
 @endsection
